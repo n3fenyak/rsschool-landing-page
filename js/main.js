8e126463eaab06a7e1a9d19275770bef17ff4462
Nikita Fenyak <nefenyak92@yandex.ru>
2026-09-22 15:00:47 +0300

    feat: update LP part 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-alert(
-  'Привет! Работа ещё в процессе. Пожалуйста, проверь позже) \n\n' +
-    'Hello! This project is still in progress. Please check little later'
-)
+const themeSwitcher = document.querySelector('.theme-switcher')
+
+const savedTheme = localStorage.getItem('theme')
+
+if (savedTheme === 'dark') {
+  document.documentElement.setAttribute('data-theme', 'dark')
+}
+
+themeSwitcher.addEventListener('click', () => {
+  const isDarkTheme =
+    document.documentElement.getAttribute('data-theme') === 'dark'
+
+  if (isDarkTheme) {
+    document.documentElement.removeAttribute('data-theme')
+    localStorage.setItem('theme', 'light')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+  }
+})
